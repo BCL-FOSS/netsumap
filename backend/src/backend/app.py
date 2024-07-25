d2985ff112f395cb2d63822c90546340e7ad95f1
D.B <derek.baugh1775@gmail.com>
2024-07-26 01:07:33 +0700

    poetry dependency update

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -5,12 +5,15 @@ from init_app import app
 import websocket
 from websocket import create_connection
 
+
 websocket.enableTrace = True
 ws=create_connection("ws://gitxiv.com/sockjs/212/2aczpiim/websocket")
 
 @app.post("/unifi_webhook")
 async def webhook():
     ws.send()
+
+
 
 
 

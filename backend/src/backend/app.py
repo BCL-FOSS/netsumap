4cda516b18312a4e8193cbcd874c34baf2f1ba04
D.B <derek.baugh1775@gmail.com>
2024-07-26 13:48:05 +0700

    Update app.py

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -10,7 +10,14 @@ ws=create_connection("ws://45.63.53.182:30000/ws")
 
 @app.post("/unifi_webhook")
 async def webhook():
-    ws.send("Message from backend. Backend -> Websocket")
+    try:
+        await ws.send("Message from backend. Backend -> Websocket")
+    except Exception as e:
+        print(e)
+        return {'Error' : e}
+    finally:
+        return {'Success?' : 'Check the websocket UI'}
+
 
 def run() -> None:
     app.run()

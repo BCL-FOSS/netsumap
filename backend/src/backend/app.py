cf747a245aeb93ef634ce0a2ce03ac43e71cb25e
D.B <derek.baugh1775@gmail.com>
2024-09-08 20:02:29 +0700

    Update app.py

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -13,16 +13,34 @@ import time
 @app.post("/unifi_auth")
 async def ubnt_auth():
     try:
-        data = request.get_json()
-        
-        dump = jsonify(data)
-            
-        unifi_profile = generate_ubiquipy_profile(ip=str(dump.get_json()['ip']), port=str(dump.get_json()['port']), user_name=str(dump.get_json()['username']), pass_word=str(dump.get_json()['password']))
+        my_tasks = set()
 
-        return unifi_profile
-            
+        task_data = asyncio.create_task(request.get_json())
+        my_tasks.add(task_data)
+        task_data.add_done_callback(my_tasks.discard)
+
+        if task_data.done():
+            print('Data coroutine complete')
+            dump = jsonify(task_data.result())
+
+        def sync_processor():
+
+            unifi_profile = generate_ubiquipy_profile(ip=str(dump.get_json()['ip']), port=str(dump.get_json()['port']), user_name=str(dump.get_json()['username']), pass_word=str(dump.get_json()['password']))
+            return unifi_profile
+
+        task_result = asyncio.create_task(sync_processor())
+        my_tasks.add(task_result)
+        task_result.add_done_callback(my_tasks.discard)
+
+        if task_result.done():
+            print('Result coroutine complete')
+            result = jsonify(task_result.result())
+
+        return result
+
     except TypeError as error:
         return {'Error' :  str(error)}
+    
 
 @app.post("/unifi_webhook")
 async def webhook():
@@ -41,26 +59,20 @@ async def webhook():
         return {'Error' : e}
     finally:
         return {'try_catch_end' : 'Check the frontend UI'}
-
+    
 def generate_ubiquipy_profile(ip='', port='', user_name='', pass_word=''):
     try:
         ubnt_profile = UniFiNetAPI(controller_ip=ip, controller_port=port, username=user_name, password=pass_word)
-        ubnt_profile.authenticate()
+        id = ubnt_profile.authenticate()
     except Exception as e:
         return {'Error' : e}
-    
-    return ubnt_profile.id
+
+    return id
 
 def activate_websocket():
     websocket.enableTrace = True
     ws=create_connection(app.config['WEBSOCKET_ADDRESS'])
     return ws
 
-
 def run() -> None:
     app.run()
-
-
-
-
-

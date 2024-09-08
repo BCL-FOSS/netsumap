6cbef49cadfec423dca18d1c687f46333c89243c
D.B <derek.baugh1775@gmail.com>
2024-09-08 20:13:53 +0700

    Update app.py

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -14,6 +14,7 @@ import time
 async def ubnt_auth():
     try:
         my_tasks = set()
+        dump = {}
 
         task_data = asyncio.create_task(request.get_json())
         my_tasks.add(task_data)

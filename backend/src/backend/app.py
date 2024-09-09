f0205f8d6e74234c879d9a3ddbc209bafefaaced
D.B <derek.baugh1775@gmail.com>
2024-09-09 11:54:40 +0700

    Update app.py

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -27,10 +27,9 @@ async def ubnt_auth():
         data_value = loop.run_until_complete(request.get_data())
 
         if data_value:
-            data = literal_eval(str(data_value))
             print('Data coroutine complete')
 
-            json_data = json.dumps(data, indent=4, sort_keys=True)
+            json_data = json.dumps(data_value, indent=4, sort_keys=True)
 
             print(json_data)
 
@@ -48,7 +47,7 @@ async def ubnt_auth():
         #    print(jsonify(result_value))
 
     except TypeError as error:
-        if " The response value returned by the view function cannot be None" in error:
+        if " The response value returned by the view function cannot be None" in str(error):
             pass
         else:
             return {'TypeError' :  str(error)}

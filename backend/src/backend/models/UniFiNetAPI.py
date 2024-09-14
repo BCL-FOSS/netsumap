a2f0def5f7b86e299c7c4ef06d036ba12f7d7685
D.B <derek.baugh1775@gmail.com>
2024-09-14 16:36:23 +0700

    Update UniFiNetAPI.py

--- backend/src/backend/models/UniFiNetAPI.py
+++ backend/src/backend/models/UniFiNetAPI.py
@@ -36,7 +36,6 @@ class UniFiNetAPI:
         except Exception as e:
             return {"status_msg": "ID Gen Failed",
                     "status_code": e}
-        
         return str(id)
     
     def get_profile_data(self):
@@ -51,6 +50,38 @@ class UniFiNetAPI:
             "token": self.token,
             "is_udm" : self.is_udm
         }
+    
+    async def make_async_request(self, url, payload, headers, cmd=''):
+
+        async with self.ubiquipy_client_session as session:
+            try:
+                match cmd.strip():
+                    case 'p':
+                        # Asynchronous POST request to UniFi API
+                        async with session.post(url=url, json=payload, headers=headers) as response:
+                            if response.status == 200:
+                                response_data = await response.json()
+                                response.close()
+                                return response_data
+                            else:
+                                return {"message": "Error", "status_code": response.status}
+                    case 'g':
+                        # Asynchronous GET request to UniFi API
+                        async with session.get(url=url, json=payload, headers=headers) as response:
+                            if response.status == 200:
+                                response_data = await response.json()
+                                response.close()
+                                return response_data
+                            else:
+                                return {"message": "Error", "status_code": response.status}
+                
+            except aiohttp.ClientError as e:
+                return {"error": str(e), "status_code": 500}
+            except Exception as error:
+                return {"error": str(error)}
+            
+
+            
 
     async def authenticate(self):
 
@@ -105,22 +136,30 @@ class UniFiNetAPI:
                         'Content-Type':'application/json',
                         'Cookie':self.token
                     }   
+        
+        data = await self.make_async_request(url=url, payload=payload, headers=headers, cmd='p')
 
-        async with self.ubiquipy_client_session as session:
-            try:
+        if data['message']:
+            return {"message": "Signout failed", "status_code": data}
+        else:
+            self.auth_check = False
+            return {"message": "Signout successful", "data": data}
+
+        #async with self.ubiquipy_client_session as session:
+        #    try:
                 # Asynchronous POST request to UniFi API
-                async with session.post(url=url, json=payload, headers=headers) as response:
-                    if response.status == 200:
-                        response_data = await response.json()
-                        self.auth_check = False
-                        response.close()
-                        return {"message": "Signout successful", "data": response_data}
-                    else:
-                        return {"message": "Signout failed", "status_code": response.status}
-            except aiohttp.ClientError as e:
-                return {"error": str(e), "status_code": 500}
-            except Exception as error:
-                return {"error": str(error)}
+        #        async with session.post(url=url, json=payload, headers=headers) as response:
+        #            if response.status == 200:
+        #                response_data = await response.json()
+        #                self.auth_check = False
+        #                response.close()
+        #                return {"message": "Signout successful", "data": response_data}
+        #            else:
+        #                return {"message": "Signout failed", "status_code": response.status}
+        #    except aiohttp.ClientError as e:
+        #        return {"error": str(e), "status_code": 500}
+        #    except Exception as error:
+        #        return {"error": str(error)}
             
     def site_dpi_data(self, site='', type=False, cmd=''):
 

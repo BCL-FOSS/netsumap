57b1e692db0ee59171413f27e0011e9be74ad24d
D.B <derek.baugh1775@gmail.com>
2024-09-15 21:43:45 +0700

    app ui buildout, backend integration

---
+++ app/netifidash/common/src/main/java/com/bcl/netifidash/ui/UI.java
@@ -0,0 +1,118 @@
+package com.bcl.netifidash.ui;
+
+import com.codename1.io.Preferences;
+import com.codename1.ui.*;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.util.UITimer;
+import com.bcl.netifidash.util.BackendUtil;
+
+public class UI {
+
+    private final BackendUtil backendUtil;
+
+    public UI (){
+        this.backendUtil = new BackendUtil();
+    }
+
+    public void splashScreen(){
+
+        String isConfigured = Preferences.get("is_configured", "");
+        Form main = new Form("", BoxLayout.y());
+        Form initSetup = new Form("", BoxLayout.y());
+
+        String configCheck = "SET";
+
+        TextArea name = new TextArea();
+        name.setText("Welcome to NetiFiDash");
+        main.add(name);
+        main.show();
+
+
+        if (isConfigured.equalsIgnoreCase(configCheck)) {
+            new UITimer(() -> {
+                Form mainApp = home();
+                mainApp.show();
+            }).schedule(3000, false, main);
+        } else {
+            new UITimer(() -> {
+                Form initApp = setup();
+                initApp.show();
+            }).schedule(3000, false, main);
+
+        }
+
+    }
+
+    public Form setup(){
+        Form setup = new Form("Setup", BoxLayout.y());
+        Button ubiquipyConnect = new Button("Connect");
+        TextField ndIP = new TextField("NetiFi Node Hostname/IP");
+        TextField ndPort = new TextField("NetiFi Node Port");
+        TextField controllerIP = new TextField("Controller Hostname/IP");
+        TextField controllerPort = new TextField("Controller Port");
+        TextField userName = new TextField("Controller Username");
+        TextField passWord = new TextField("Controller Password");
+
+        ubiquipyConnect.addActionListener(e -> this.backendUtil.connect(ndIP.getText(), ndPort.getText(), controllerIP.getText(), controllerPort.getText(),
+                userName.getText(), passWord.getText()));
+
+        setup.add(ndIP);
+        setup.add(ndPort);
+        setup.add(controllerIP);
+        setup.add(controllerPort);
+        setup.add(userName);
+        setup.add(passWord);
+        setup.add(ubiquipyConnect);
+
+        return setup;
+    }
+
+    public Form home(){
+        Form home = new Form("Home", BoxLayout.y());
+        Button socketTest = new Button("Send message to backend");
+        TextField eventType = new TextField("Enter Event Type");
+        TextField eventMessage = new TextField("Enter Event Message");
+        TextField socketHost = new TextField("Enter Socket IP/Hostname");
+        TextField socketPort = new TextField("Enter Socket Port");
+        home.add(socketHost);
+        home.add(socketPort);
+        home.add(eventType);
+        home.add(eventMessage);
+        socketTest.addActionListener(e -> nocDash());
+        home.add(socketTest);
+        setMenu(home);
+        return home;
+    }
+
+    public void nocDash(){
+        Form noc = new Form("Dashboard", BoxLayout.y());
+        setMenu(noc);
+        noc.show();
+    }
+
+    public void net_dev_mgmt(){
+        Form netDev = new Form("Network Devices", BoxLayout.y());
+        setMenu(netDev);
+        netDev.show();
+    }
+
+    public void client_mgmt(){
+        Form usrDev = new Form("Client Devices", BoxLayout.y());
+        setMenu(usrDev);
+        usrDev.show();
+
+    }
+
+    public void setMenu(Form form){
+
+        form.getToolbar().addMaterialCommandToSideMenu("Home",
+                FontImage.MATERIAL_CHECK_CIRCLE_OUTLINE, 6, e -> home().show());
+        form.getToolbar().addMaterialCommandToSideMenu("Dashboard",
+                FontImage.MATERIAL_CHECK_CIRCLE_OUTLINE, 6, e -> nocDash());
+        form.getToolbar().addMaterialCommandToSideMenu("Network Devices",
+                FontImage.MATERIAL_CHECK_CIRCLE_OUTLINE, 6, e -> net_dev_mgmt());
+        form.getToolbar().addMaterialCommandToSideMenu("Client Devices",
+                FontImage.MATERIAL_CHECK_CIRCLE_OUTLINE, 6, e -> client_mgmt());
+
+    }
+}
--- app/netifidash/common/src/main/java/com/bcl/netifidash/util/BackendUtil.java
+++ app/netifidash/common/src/main/java/com/bcl/netifidash/util/BackendUtil.java
@@ -1,33 +1,38 @@
 package com.bcl.netifidash.util;
 
 import com.codename1.components.ToastBar;
-import com.codename1.io.ConnectionRequest;
 import com.codename1.io.rest.Response;
 import com.codename1.io.rest.Rest;
-
-import java.net.http.WebSocket;
-import java.nio.ByteBuffer;
+import com.codename1.ui.Dialog;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 
 public class BackendUtil {
-    public BackendUtil (){
 
-    }
+    public BackendUtil (){}
 
-    public void login(){
-        Response<Map> result = Rest.post("http://107.191.44.222:25000/unifi_webhook").
-                contentType("application/json").body("{\"unifi_alarm\" : \"from_mobile_app\"}")
+    public void connect(String nd_ip, String nd_port, String ip, String port, String user, String pass){
+
+        String baseURL = "http://"+nd_ip+":"+nd_port;
+
+        Map<String, String> json = new LinkedHashMap<>();
+        json.put("port", port);
+        json.put("ip", ip);
+        json.put("username", user);
+        json.put("password", pass);
+
+        Response<Map> result = Rest.post(baseURL+"/login").
+                contentType("application/json").body(json.toString())
                 .getAsJsonMap();
-
 
         if(result.getResponseData() != null) {
             String error = (String)result.getResponseData().get("error_message");
             if(error != null) {
-                ToastBar.showErrorMessage(error);
+                Dialog.show("UniFi Connection Error", error, "OK", null);
             }
         } else {
-            ToastBar.showErrorMessage("Error connecting to Backend API: " + result.getResponseCode());
+            Dialog.show("UniFi Connection Successful", result.getResponseData().toString(), "OK", null);
+
         }
     }
 

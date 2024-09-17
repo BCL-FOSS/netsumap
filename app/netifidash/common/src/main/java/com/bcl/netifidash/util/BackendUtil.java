c2bb459f4ae12d11d42f0247b26fe24f2fa1091b
D.B <derek.baugh1775@gmail.com>
2024-09-17 16:54:10 +0700

    Update BackendUtil.java

--- app/netifidash/common/src/main/java/com/bcl/netifidash/util/BackendUtil.java
+++ app/netifidash/common/src/main/java/com/bcl/netifidash/util/BackendUtil.java
@@ -16,28 +16,42 @@ public class BackendUtil {
 
     public void connect(String nd_ip, String nd_port, String ip, String port, String user, String pass){
 
-        String baseURL = "http://"+nd_ip+":"+nd_port;
+        try {
+            //  Block of code to try
+            String baseURL = "http://"+nd_ip+":"+nd_port;
 
-        Map<String, String> json = new LinkedHashMap<>();
-        json.put("port", port);
-        json.put("ip", ip);
-        json.put("username", user);
-        json.put("password", pass);
+            Map<String, String> json = new LinkedHashMap<>();
+            json.put("port", port);
+            json.put("ip", ip);
+            json.put("username", user);
+            json.put("password", pass);
 
-        //System.out.println(json.toString());
+            //System.out.println(json.toString());
 
-        Response<Map> result = Rest.post(baseURL+"/login").
-                contentType("application/json").body(json.toString())
-                .getAsJsonMap();
+            Response<String> result = Rest.post(baseURL+"/login").jsonContent()
+                    .body(json.toString())
+                    .getAsString();
 
-        if(result.getResponseData() != null) {
-            String error = (String)result.getResponseData().get("error_message");
-            if(error != null) {
-                Dialog.show("UniFi Connection Error", error, "OK", null);
+            if(result.getResponseCode() == 200) {
+                Dialog.show("UniFi Connection Successful", result.getResponseData(), "OK", null);
+
+
+            } else {
+                Dialog.show("UniFi Connection Failed", result.getResponseData(), "OK", null);
+
             }
-        } else {
-            Dialog.show("UniFi Connection Successful", result.getResponseData().toString(), "OK", null);
+
         }
+        catch(Exception e) {
+            //  Block of code to handle errors
+
+            Dialog.show("UniFi Connection Error", e.toString(), "OK", null);
+
+
+            System.out.println(e.toString());
+        }
+
+
     }
 
     public void auth(){

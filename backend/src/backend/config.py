f773e964bf3d7945b5ca90b599da133780d88621
D.B <derek.baugh1775@gmail.com>
2024-10-17 22:51:38 +0700

    Update config.py

--- backend/src/backend/config.py
+++ backend/src/backend/config.py
@@ -7,7 +7,7 @@
 SECRET_KEY='UN2gP0OU02ItHTVPdF_F9g'
 
 # Path to IP Classification Keras Model
-MODEL='malicious_ip_multiclass_classification_model.keras'
+MODEL='/home/appadmin/ai-threat-mgmt/malicious_ip_multiclass_classification_model.keras'
 
 # Redis DB Connection Information
 REDIS_DB="localhost"

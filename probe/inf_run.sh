f553ec5c690c1a0cd1db2c55ee18bdeb4a309b40
D.B <derek.baugh1775@gmail.com>
2024-11-08 19:52:37 +0700

    Update inf_run.sh

--- probe/inf_run.sh
+++ probe/inf_run.sh
@@ -4,15 +4,9 @@
 # run example:
 # 	./inf_run.sh
 
-# Prompt user for the Python script directory
-echo "PARAM FORMAT Ex.:http://0.0.0.0:25000 150" 
-echo "nmp_ip (netsumap-core IP/Hostname:Port)" 
-echo "pcap_count (Num packets to capture per run)"\n
-
-read -p "Enter script params: " INF_PARAMS
+read INF_PARAMS
 
 # Start venv
 . .venv/bin/activate 
-
 
 /usr/bin/python3 collect.py $INF_PARAMS

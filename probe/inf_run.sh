49753dbd02a7c6082f49024c9d855868655ee136
D.B <derek.baugh1775@gmail.com>
2024-11-08 19:22:45 +0700

    Update inf_run.sh

--- probe/inf_run.sh
+++ probe/inf_run.sh
@@ -4,15 +4,6 @@
 # run example:
 # 	./inf_run.sh
 
-scriptdir= $(pwd)
-scriptfile= "collect.py"
-findscript= $scriptdir$scriptfile
-# Check if script exists
-if [ ! -f $findscript]; then
-    echo "Error: File does not exist"
-    exit 1
-fi
-
 # Prompt user for the Python script directory
 echo "PARAM FORMAT Ex.:http://0.0.0.0:25000 150" 
 echo "nmp_ip (netsumap-core IP/Hostname:Port)" 
@@ -23,5 +14,5 @@ read -p "Enter script params: " INF_PARAMS
 # Start venv
 . .venv/bin/activate 
 
-# Run collector.py
-/usr/bin/python3 $findscript $INF_PARAMS
+
+/usr/bin/python3 collect.py $INF_PARAMS

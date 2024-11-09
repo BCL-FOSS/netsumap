0815914f50b66f37797338efbdc511be9a0dc7b3
D.B <derek.baugh1775@gmail.com>
2024-11-10 01:30:20 +0700

    Update inf_unenroll.sh

--- probe/inf_unenroll.sh
+++ probe/inf_unenroll.sh
@@ -7,9 +7,16 @@
 dot="$(cd "$(dirname "$0")"; pwd)"
 script="$dot/inf_run.sh"
 
-if sudo crontab -l | grep -v "$script"  | sudo crontab - < "$0"; then
+# Construct the cron job command
+CRON_JOB="*/5 * * * * /usr/bin/python3 $script $1 $2"
+
+# Check if the cron job already exists
+(sudo crontab -l | grep -F "$CRON_JOB") > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    sudo crontab -l | grep -v "$CRON_JOB"  | sudo crontab -
     echo "Cronjob deletion success"
 else
-    echo "Cronjob deletion failed"
-    exit 1
+    echo "Cronjob does not exist. No changes made"
 fi
+

d3faab674e2b41e9fbf00365fc453655f148dcc3
D.B <derek.baugh1775@gmail.com>
2024-11-11 00:39:41 +0700

    Update netsumap_init.sh

--- core-node/netsumap_init.sh
+++ core-node/netsumap_init.sh
@@ -18,6 +18,8 @@ sudo apt-get update -y
 # Install Docker
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
+sudo apt install docker-compose
+
 docker volume create netsumap_caddy_data
 
 docker-compose build && docker-compose up

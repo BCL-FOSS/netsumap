d53b350798631011be2e7e9ecea8612b1f314b40
D.B <derek.baugh1775@gmail.com>
2024-11-12 04:32:34 +0700

    Update app.py

--- core-node/quartapp/app.py
+++ core-node/quartapp/app.py
@@ -136,7 +136,8 @@ async def probe_registration():
 
             print(probe_json, flush=True)
 
-            print(probe_json['id'], flush=True)
+            for key in probe_json:
+                print(key, flush=True)
 
             #db_upload = await db.upload_db_data(id=probe_id, data=probe_data)
             #print(db_upload, flush=True)

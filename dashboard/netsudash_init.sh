a6a9036757423d1789284b3d3e61a6c636ebf2d2
D.B <derek.baugh1775@gmail.com>
2024-11-13 02:26:30 +0700

    init updates

--- dashboard/netsudash_init.sh
+++ dashboard/netsudash_init.sh
@@ -3,20 +3,6 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-
-sudo apt-get install ca-certificates curl -y
-sudo install -m 0755 -d /etc/apt/keyrings -y
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
 # Install Docker
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
@@ -27,5 +13,5 @@ docker compose build --no-cache
 docker volume create --name=caddy_data
 docker volume create --name=caddy_config
 
-docker compose up
+docker compose up -d
 

0c76d9f49054ae13359c4761ab7b2e534db5df20
D.B <derek.baugh1775@gmail.com>
2024-11-17 21:18:04 +0700

    ui test

--- core-node/quartapp/app.py
+++ core-node/quartapp/app.py
@@ -40,7 +40,7 @@ async def handle_internal_error(e):
 
 @app.get("/")
 async def index():
-    return await render_template("dashboard.html", test_func=test_func)
+    return await render_template("index.html")
 
 @app.get("/dashboard")
 async def dash():

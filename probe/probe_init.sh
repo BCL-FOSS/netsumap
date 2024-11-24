be4c7a6afa762cba60cb6d27eddd77739d87bdb4
D.B <derek.baugh1775@gmail.com>
2024-11-24 18:35:54 +0700

    probe init update

--- probe/probe_init.sh
+++ probe/probe_init.sh
@@ -7,7 +7,7 @@ sudo apt-get upgrade -y
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 sudo apt-get install ca-certificates curl -y
-sudo install -m 0755 -d /etc/apt/keyrings -y
+sudo install -m 0755 -d /etc/apt/keyrings 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
@@ -19,7 +19,7 @@ echo \
 sudo apt-get update -y
 
 # Install Docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 sudo apt install docker-compose -y
 

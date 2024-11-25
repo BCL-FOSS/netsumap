caa67a37d52a6a9352130513b3f2074406bcee87
D.B <derek.baugh1775@gmail.com>
2024-11-25 15:54:32 +0700

    update

--- probe/quartapp/app.py
+++ probe/quartapp/app.py
@@ -6,6 +6,12 @@ from models.ProbeNetwork import ProbeNetwork
 import threading
 import iperf3
 import os
+import sqlite3
+
+folder_name = 'probe_data'
+data_dir_path = os.path.join(app.instance_path, folder_name)
+main_network = ProbeNetwork()
+core_conn = NetsumapCoreConn()
 
 @app.before_serving
 async def start_iperf_server():
@@ -13,12 +19,33 @@ async def start_iperf_server():
     threading.Thread(target=run_iperf_server, daemon=True).start()
 
 @app.before_serving
+async def probe_data_dir():
+    # Create probe data folder 
+    if os.path.isdir(data_dir_path) is False:
+        os.makedirs(data_dir_path, exist_ok=True)
+        print(f"Probe data directory {data_dir_path} created successfully", flush=True)
+    else:
+        pass
+
+@app.before_serving
 async def register_probe():
-    network = ProbeNetwork()
-    core_conn = NetsumapCoreConn()
-    ports = network.open_tcp_ports()
-    external_ip = network.get_public_ip()
-    core_conn.register(url=os.getenv("CORE_NAME", ""), public_ip=external_ip, USE_DB=app.config['USE_DB'],ports=ports)
+    app.config['USE_DB'] = True
+    db_path = db_path = os.path.join(data_dir_path, 'probe.db')
+
+    if os.path.exists(db_path) == False:
+        conn = sqlite3.connect(db_path)
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE pbdata(id, status, host_ip, hostname)")
+        res = cur.execute("SELECT name FROM sqlite_master")
+        if res.fetchone() is None:
+            print('Failed to create table in db', flush=True)
+            app.config['USE_DB']=False
+    else:
+        print("Probe DB already exists", flush=True)
+        
+    listening_ports = main_network.open_tcp_ports()
+    external_ip = main_network.get_public_ip()
+    core_conn.register(url=os.getenv("CORE_NAME"), public_ip=external_ip, USE_DB=app.config['USE_DB'],ports=listening_ports)
 
 @app.errorhandler(404)
 async def page_not_found():
@@ -36,8 +63,7 @@ async def bandwidth_test():
 def run_iperf_server():
     """Start the iPerf3 server on a separate thread."""
     server = iperf3.Server()
-    network = ProbeNetwork()
-    external_ip = network.get_public_ip()
+    external_ip = main_network.get_public_ip()
     server.bind_address = external_ip
     iperf_port = int(os.getenv("IPERF_PORT"))
     server.port = iperf_port

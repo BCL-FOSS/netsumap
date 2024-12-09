2c9d3c1e46b2e9951f9e31c1b9b3ca60c54c9bed
D.B <derek.baugh1775@gmail.com>
2024-12-09 20:22:10 +0700

    Update app.py

--- probe/probe_main/app.py
+++ probe/probe_main/app.py
@@ -8,6 +8,27 @@ main_network = app.config['NETWORK_OBJ']
 probe = app.config['PROBE_OBJ']
 core_conn = app.config['CORE_CONN']
 
+@app.post("/probe_init")
+def probe_init():
+    register_check()
+
+@app.post("/pcap_init")
+def pcap_init():
+    scan_options = request.get_json(silent=True)
+    if scan_options is not None:
+        main_network.pcap_scan(iface=scan_options['iface'], count=scan_options['count'])
+
+@app.errorhandler(404)
+def page_not_found():
+    return jsonify({"error": "Resource not found or does not exist"}), 404
+
+@app.errorhandler(500)
+def handle_internal_error(e):
+    return jsonify({"error": "Internal server error"}), 500
+
+def run() -> None:
+    app.run()
+
 def register_check():
     """Check probe registration status."""
 
@@ -39,24 +60,3 @@ def register_check():
             db_query_value = db.get_obj_data(key=probe_id)
             if db_query_value:
                print(db_query_value, flush=True)
-
-@app.post("/probe_init")
-def probe_init():
-    register_check()
-
-@app.post("/pcap_init")
-def pcap_init():
-    scan_options = request.get_json(silent=True)
-    if scan_options is not None:
-        main_network.pcap_scan(iface=scan_options['iface'], count=scan_options['count'])
-
-@app.errorhandler(404)
-def page_not_found():
-    return jsonify({"error": "Resource not found or does not exist"}), 404
-
-@app.errorhandler(500)
-def handle_internal_error(e):
-    return jsonify({"error": "Internal server error"}), 500
-
-def run() -> None:
-    app.run()
